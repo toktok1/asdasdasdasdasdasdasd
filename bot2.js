c41cd4b17d615f0d24109132dbb71c7d0c12b2a4
toktok1 <43493712+toktok1@users.noreply.github.com>
2018-12-31 18:54:12 +0200

    Create bot2.js

---
+++ bot2.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Saske");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("529333409734983689")
+setInterval(function() {
+channel.send(`  gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand:   gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand:   gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand:   gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand:   gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand:   gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: gg gg gg gg gg :ok_hand: `);
+}, 25)
+})
+ 
+ 
+client.login(process.env.BOT_TOKEN2);
