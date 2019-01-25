6a3fbfe4bca555c364ae94e3d63fb47c881d1b7f
toktok1 <43493712+toktok1@users.noreply.github.com>
2019-01-25 14:45:49 +0200

    Create bot8.js

---
+++ bot8.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.on('guildMemberAdd', member => {
+
+    const channel = member.guild.channels.find('name', '・aa');
+  
+    const millis = new Date().getTime() - member.user.createdAt.getTime();
+    const now = new Date();
+    const createdAt = millis / 1000 / 60 / 60 / 24;
+
+
+
+
+  
+    const embed = new Discord.RichEmbed()
+    
+    .setColor("black")
+    .setDescription(`**تاريخ دخولك للدسكورد منذ ${createdAt.toFixed(0)} يوم**`)
+    .setAuthor(member.user.tag, member.user.avatarURL);
+    channel.sendEmbed(embed);
+
+  
+});
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN8);
